52a21dd256e1337c4c93b86ceed296423828410d
JustinasBliujus <45039478+JustinasBliujus@users.noreply.github.com>
2025-10-27 20:48:08 +0200

    testing

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import process from "process";
 
 export default defineConfig({
   plugins: [react()],
   ssr: {
-    external: ["pg-hstore"], 
+    external: ["pg-hstore"],
   },
   server: {
-    port: parseInt(process.env.VITE_PORT),
+    port: 5137,
   },
 });
