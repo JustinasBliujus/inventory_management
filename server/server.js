e93c860ffe2af4306686cb7c90250d0e88aaf91a
JustinasBliujus <45039478+JustinasBliujus@users.noreply.github.com>
2025-10-28 10:33:32 +0200

    testing

--- server/server.js
+++ server/server.js
@@ -1,44 +1,50 @@
-
-import express from 'express';
-import cors from 'cors';
-import session from 'express-session';
-import cookieParser from 'cookie-parser';
-import router from './routes/router.js';
-import db from './db.js';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import router from "./routes/router.js";
+import db from "./db.js";
 
 dotenv.config();
 const app = express();
 
- app.use(cors({ 
-     origin: process.env.FRONTEND_URL, 
-     credentials: true 
- }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true, 
+  })
+);
 
- app.use(express.json());
- app.use(express.urlencoded({ extended: true }));
- app.use(bodyParser.urlencoded({extended: true}));
- app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
- db.sync()
-   .then(() => console.log("All models synchronized successfully."))
-   .catch(err => console.error("Error syncing models:", err));
+db.sync()
+  .then(() => console.log("All models synchronized successfully."))
+  .catch((err) => console.error("Error syncing models:", err));
 
- app.use(session({
-     secret: process.env.SESSION_SECRET || 'sessionkey',
-     resave: false,
-     saveUninitialized: false,
-     cookie: {
-         httpOnly: true,
-         sameSite: 'lax',
-         secure: false
-     }
- }));
+app.set("trust proxy", 1);
 
-app.use('/', router);
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "sessionkey",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,       
+      sameSite: "none",     
+      secure: true,         
+    },
+  })
+);
+
+app.use("/", router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Frontend allowed: ${process.env.FRONTEND_URL}`);
 });
